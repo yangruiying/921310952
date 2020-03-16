f774e05543d212717cd41a482b9579fba9374e3b
wheelss <2323539733@qq.com>
2020-03-16 16:30:19 +0800

    Default Changelist

--- src/Test.java
+++ src/Test.java
@@ -59,7 +59,7 @@ class homework {
         System.out.println("101-200所有素数");
         int p = 1;
         int sum = 0;
-        for (int i = 101; i <= 200; i++) {
+        for (int i = 0; i <= 200; i++) {
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     p = 0;
